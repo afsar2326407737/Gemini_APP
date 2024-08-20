683eb01ff2046d7abee38fc06e9dea97ff6a1de4
afsar2326407737 <mohamedafsar.a2022cce@sece.ac.in>
2024-08-20 19:19:43 +0530

    complete gemini Integration

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -27,7 +27,7 @@ class _HomePageState extends State<HomePage> {
     });
 
     try {
-      final apiKey = 'AIzaSyB1QI1TwdQPNRDnezeXSXKo17mHK1Bp8pM';
+      final apiKey = 'Api_key_of_your_Gemini_Ai';
       final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey);
       final content = [Content.text(query)];
 
